efa025e879b5a52fe2627b54a79dbbc03a12ca6b
J. Singleton <jennifer.singleton@gmail.com>
2019-01-26 18:11:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,13 @@
 class Artist
   attr_accessor :name
 
-  def initialize
+  def initialize(name)
+    name = @name
     @songs = []
+  end
+
+  def add_song(song)
+    Song.artist = self
+    @song << song
   end
 end
